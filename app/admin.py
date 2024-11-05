6eca78ed7aacfd523182c8fa283806e26a9147bc
oguel <miguelcustodiopereira@gmail.com>
2024-11-05 16:53:36 -0300

    att

--- app/admin.py
+++ app/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from.models import *
 
 admin.site.register(UF)
+admin.site.register(Cidade)
+admin.site.register(Usuario)
+admin.site.register(Genero)
+admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
+admin.site.register(Emprestimo)
--- app/models.py
+++ app/models.py
@@ -1,9 +1,66 @@
 from django.db import models
-class UF(models.Model):
-    sigla= models.CharField(max_length=2)
 
-    class Meta:
-        verbose_name = 'Unidade Federativa'
-        verbose_name_plural = 'Unidades Federais'
+class UF(models.Model):
+    sigla = models.CharField(max_length=2, unique=True)
+
     def __str__(self):
         return self.sigla
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=100)
+    uf = models.ForeignKey(UF, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nome} - {self.uf}"
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True)
+    data_nasc = models.DateField()
+    email = models.EmailField(unique=True)
+    telefone = models.CharField(max_length=15)
+    cidade = models.ForeignKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    site = models.URLField()
+    cidade = models.ForeignKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    cidade = models.ForeignKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Livro(models.Model):
+    nome = models.CharField(max_length=200)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    data_publicacao = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+class Emprestimo(models.Model):
+    data_emprestimo = models.DateField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    data_devolucao = models.DateField()
+
+    def __str__(self):
+        return f"Empréstimo de {self.livro} para {self.usuario} em {self.data_emprestimo}"

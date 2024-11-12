3eb231aad5aedc7242bda7109316645b6b6e224f
Miguel Custodio Pereira <09286210655@muzambinho.edu.br>
2024-11-12 15:53:40 -0300

    herança e inline

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,40 @@
 from django.contrib import admin
-from.models import *
+from .models import *
 
-admin.site.register(UF)
+class CidadeInline(admin.TabularInline):
+    model = Cidade
+    extra = 1
+
+class UFAdmin(admin.ModelAdmin):
+    list_display = ('sigla',)
+    search_fields = ('sigla',)
+    inlines = [CidadeInline]
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cidade')
+    search_fields = ('nome', 'cidade__nome')
+
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cidade')
+    search_fields = ('nome', 'cidade__nome')
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cidade')
+    search_fields = ('nome', 'cidade__nome')
+
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'data_emprestimo', 'data_devolucao')
+    search_fields = ('usuario__nome',)
+
+admin.site.register(UF, UFAdmin)
 admin.site.register(Cidade)
-admin.site.register(Usuario)
-admin.site.register(Genero)
-admin.site.register(Editora)
-admin.site.register(Autor)
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Genero, GeneroAdmin)
+admin.site.register(Editora, EditoraAdmin)
+admin.site.register(Autor, AutorAdmin)
 admin.site.register(Livro)
-admin.site.register(Emprestimo)
+admin.site.register(Emprestimo, EmprestimoAdmin)
--- app/models.py
+++ app/models.py
@@ -12,38 +12,70 @@ class Cidade(models.Model):
 
     def __str__(self):
         return f"{self.nome} - {self.uf}"
+    
+class Pessoa(models.Model):
+        nome = models.CharField(max_length=100, default='')
+        email = models.CharField(max_length=100, default='')
+        cidade = models.ForeignKey(Cidade,on_delete=models.CASCADE, null=True, blank=True)
+        telefone = models.IntegerField(default=0)
 
-class Usuario(models.Model):
-    nome = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11, unique=True)
-    data_nasc = models.DateField()
-    email = models.EmailField(unique=True)
-    telefone = models.CharField(max_length=15)
-    cidade = models.ForeignKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
+        class Meta:
+            abstract = True
+        def __str__(self):
+            return self.nome
+    
 
-    def __str__(self):
-        return self.nome
+class Usuario(Pessoa):
+    pass
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+        
 
 class Genero(models.Model):
     nome = models.CharField(max_length=100, unique=True)
 
+# Superclass de 'Pessoa' que sera uma classe pai para: PessoaFisica e Pessoa Juridica
+class PessoaFisica(Pessoa):
+        cpf = models.IntegerField(default=0)
+        data_nasc = models.DateField(default='2000-01-01',verbose_name='Data de Nascimento')
+        
+
+        class Meta:
+           abstract = True
+        
+        def __str__(self): 
+            return self.nome
+   
+class PessoaJuridica(Pessoa):
+        cnpj = models.IntegerField(default=0)
+        razao_social = models.CharField(max_length=100, default='')
+        data_fundacao = models.DateField(default='2000-01-01',verbose_name='Data de Nascimento')
+
+        class Meta:
+         abstract = True
+
+         def __str__(self): 
+            return self.nome
+    
+class Editora(PessoaJuridica):
+    site = models.CharField()
+
+    class Meta:
+        verbose_name = "Editora"
+        verbose_name_plural = "Editoras"
+
+
+class Autor(PessoaFisica):
+
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
+
     def __str__(self):
         return self.nome
 
-class Editora(models.Model):
-    nome = models.CharField(max_length=100, unique=True)
-    site = models.URLField()
-    cidade = models.ForeignKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
-
-    def __str__(self):
-        return self.nome
-
-class Autor(models.Model):
-    nome = models.CharField(max_length=100)
-    cidade = models.ForeignKey(Cidade, on_delete=models.SET_NULL, null=True, blank=True)
-
-    def __str__(self):
-        return self.nome
 
 class Livro(models.Model):
     nome = models.CharField(max_length=200)
@@ -51,16 +83,16 @@ class Livro(models.Model):
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
     editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    data_publicacao = models.DateField()
+    data_publicacao = models.DateField(default='2000-01-01')
 
     def __str__(self):
         return self.nome
 
 class Emprestimo(models.Model):
-    data_emprestimo = models.DateField()
+    data_emprestimo = models.DateField(default='2000-01-01')
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
-    data_devolucao = models.DateField()
+    data_devolucao = models.DateField(default='2000-01-01')
 
     def __str__(self):
         return f"Empréstimo de {self.livro} para {self.usuario} em {self.data_emprestimo}"
